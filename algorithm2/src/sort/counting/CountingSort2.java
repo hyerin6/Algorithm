14c3334011ef32b8d7ab8f5b82b592eaf65cbb17
hyerin6 <hyerinn6@gmail.com>
2019-04-08 21:47:39 +0900

    4/8

--- algorithm2/src/sort/counting/CountingSort2.java
+++ algorithm2/src/sort/counting/CountingSort2.java
@@ -10,7 +10,7 @@ public class CountingSort2 {
 		int[] counts = new int['z' - 'a' + 1];
 
 		for (char c : a) // 각 문자의 수를 센다
-			++counts[c - 'a'];
+			++counts[c - 'a']; // counts[c - 'a'] += 1;
 
 		int index = 0; // 배열에 값을 채우기 위한 인덱스
 		for (int i = 0; i < counts.length; ++i) // 각 문자의 수 만큼 배열에 채운다
