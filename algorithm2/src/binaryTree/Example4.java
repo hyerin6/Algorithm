825471fb2df79f12b049bed5a4ccf1a83408c224
hyerin6 <hyerinn6@gmail.com>
2019-04-10 01:20:18 +0900

    4/9

--- algorithm2/src/binaryTree/Example4.java
+++ algorithm2/src/binaryTree/Example4.java
@@ -21,7 +21,7 @@ public class Example4 {
 			this.right = null;
 		}
 
-		// 이진 탐색 트리는 중복을 허용하지 않기 문에 
+		// 이진 탐색 트리는 중복을 허용하지 않기 때문에 
 		// (value == this.value)인 경우 add 하지 않고 무시한다. 
 		public void add(int value) {
 			if(this.value > value) {
@@ -54,20 +54,19 @@ public class Example4 {
 		}
 
 		public void remove(int value, Node parent) {
-			if(value < this.value) { 								// remove하고 싶은 값이 현재 노드 보다 작으면 	
-				if(left != null) left.remove(value, this);			// 왼쪽 서브 트리에서 remove()
-			}else if(value > this.value) {							// remove하고 싶은 값이 현재 노드 보다 크면
-				if(right != null) right.remove(value, this);		// 오른쪽 서브 트리에서 remove()
-			}else { 												// value 값이 this.value와 같은 경우에 이 코드가 실행된다.
-				if (left != null && right != null) {				// this를 삭제해야 한다.
-					int temp = right.getLeftMostValue();			// this.value 다음으로 큰 값을 찾는다. 
-					this.value = temp;								// 그 값을 this.value에 대입한다.
-					right.remove(temp, this);						// 그 값의 노드를 오른쪽 서브트리에서 remove 한다. (재귀 호출)
-				}else {												// this 노드에 적어도 자식이 하나 없는 경우 실행
-					Node child = (left != null) ? left : right;		// left, right 중 null이 아닌 값을 child 변수에 대입, 둘 다 null 이라면 child 변수에 null을 대입
+			if(value < this.value) { 				// remove하고 싶은 값이 현재 노드 보다 작으면 	
+				if(left != null) left.remove(value, this);	// 왼쪽 서브 트리에서 remove()
+			}else if(value > this.value) {				// remove하고 싶은 값이 현재 노드 보다 크면
+				if(right != null) right.remove(value, this);	// 오른쪽 서브 트리에서 remove()
+			}else { 						// value 값이 this.value와 같은 경우에 이 코드가 실행된다.
+				if (left != null && right != null) {		// this를 삭제해야 한다.
+					int temp = right.getLeftMostValue();	// this.value 다음으로 큰 값을 찾는다. 
+					this.value = temp;			// 그 값을 this.value에 대입한다.
+					right.remove(temp, this);		// 그 값의 노드를 오른쪽 서브트리에서 remove 한다. (재귀 호출)
+				}else {						// this 노드에 적어도 자식이 하나 없는 경우 실행
+					Node child = (left != null) ? left : right;	// left, right 중 null이 아닌 값을 child 변수에 대입, 둘 다 null 이라면 child 변수에 null을 대입
 					if (parent.left == this) parent.left = child;	// this 노드가 parent의 left 자식이라면, parent.left = child;
-					else parent.right = child;						// this 노드가 parent의 right 자식이라면, parent.right = child;
-				}
+					else parent.right = child;			// this 노드가 parent의 right 자식이라면, parent.right = child;
 			}
 		}
 	}
