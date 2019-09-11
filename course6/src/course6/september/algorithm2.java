b723f3eed6c9f1c8824aa6742f061fc33b933b61
hyerin6 <hyerinn6@gmail.com>
2019-09-11 23:54:41 +0900

    과정지도6 구현문제2

---
+++ course6/src/course6/september/algorithm2.java
@@ -0,0 +1,19 @@
+// 2. 구현
+package course6.september;
+
+public class algorithm2 {
+
+	public static int solution(String str) {
+		String[] arr = str.split("");
+		
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(solution("aabbaccc"));
+
+	}
+
+}
