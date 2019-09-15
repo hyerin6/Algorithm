3497d6671ffbba219817e87f84e307956368f77a
hyerin6 <hyerinn6@gmail.com>
2019-09-15 21:46:25 +0900

    과정지도6

--- course6/src/course6/september/algorithm2.java
+++ course6/src/course6/september/algorithm2.java
@@ -1,18 +1,41 @@
 // 2. 구현
 package course6.september;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class algorithm2 {
 
 	public static int solution(String str) {
 		String[] arr = str.split("");
+		Map<String, Integer> map = new HashMap<>();
+		String maxValue = ""; 
+		int maxCount = 0, maxValueIndex = 0;
+
+		for(int i = 0; i < arr.length - 1; ++i) {
+			
+			if()
+			
+			// map.put(arr[i], count == null ? 1 : count + 1);
+			
+			if(map.get(arr[i]) > maxCount) {
+				maxCount = map.get(arr[i]);
+				maxValue = arr[i];
+				maxValueIndex = i;
+			}
+		}
 		
-		return 1;
+
+		
+		
+		
+		return maxValueIndex;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(solution("aabbaccc"));
+		solution("aabbaccc");
 
 	}
 
