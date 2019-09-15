cd1b060d01b3e1ffb6b903731fc59cb9658e8a3a
hyerin6 <hyerinn6@gmail.com>
2019-09-15 21:56:21 +0900

    과정지도6

--- course6/src/course6/september/algorithm2.java
+++ course6/src/course6/september/algorithm2.java
@@ -7,29 +7,23 @@ import java.util.Map;
 public class algorithm2 {
 
 	public static int solution(String str) {
+		int result = 0;
 		String[] arr = str.split("");
-		Map<String, Integer> map = new HashMap<>();
-		String maxValue = ""; 
-		int maxCount = 0, maxValueIndex = 0;
-
-		for(int i = 0; i < arr.length - 1; ++i) {
-			
-			if()
-			
-			// map.put(arr[i], count == null ? 1 : count + 1);
-			
-			if(map.get(arr[i]) > maxCount) {
-				maxCount = map.get(arr[i]);
-				maxValue = arr[i];
-				maxValueIndex = i;
-			}
+		int len = str.length();
+		
+		int count = 0;
+		String[] countStr;
+		
+		for(int i = 1; i < len-1; ++i) {
+			// if(arr[0] == )
 		}
 		
-
 		
 		
 		
-		return maxValueIndex;
+		
+		
+		return result;
 	}
 
 	public static void main(String[] args) {
