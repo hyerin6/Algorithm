b767d17ec80459123882efe74ce2c737c8ec0c79
hyerin6 <hyerinn6@gmail.com>
2019-09-19 23:45:56 +0900

    구현 2

--- course6/src/course6/september/Algorithm2.java
+++ course6/src/course6/september/Algorithm2.java
@@ -5,38 +5,24 @@ public class Algorithm2 {
 
 	public static int solution(String str) {
 		int result = 0;
-
+		int[] count;
+		String[] arr = str.split("");
+		
+		for(int i = 0; i < arr.length; ++i) {
+			for(int j = 0; j < arr.length; ++j) {
+				String[] s = null;
+			
+			}
+	
+		}
+		
 		return result;
 	}
 
 	// 같은 문자가 반복되면 횟수 표시하여 문자열 압축
 	public static String compress(String s, int startIndex, int endIndex) {
-		String inputTxt = s;
  		String resultTxt = "";
-        int count = 0;
-        
-        
-        String temp = inputTxt.substring(startIndex, endIndex);
-        count++;
-        
-        for ( int i = 1; i < inputTxt.length(); i++ ) {
-            
-            if ( inputTxt.substring(startIndex) != temp ) {
-                resultTxt += temp + "" + count;
-                count = 0;
-                
-                temp = inputTxt.substring(startIndex);
-                count++;
-                
-            } else {
-                count++;
-            }
-            
-            if ( i == inputTxt.length()-1 ) {
-                resultTxt += temp + "" + count;
-            }
-        }
-        
+      
         return resultTxt;
 	}
 
