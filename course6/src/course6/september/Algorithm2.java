1609dceeef71907fc0576f5026dc26ea77422375
hyerin6 <hyerinn6@gmail.com>
2019-09-19 23:09:43 +0900

    구현 2번

--- course6/src/course6/september/Algorithm2.java
+++ course6/src/course6/september/Algorithm2.java
@@ -5,25 +5,44 @@ public class Algorithm2 {
 
 	public static int solution(String str) {
 		int result = 0;
-		Algorithm2 obj = new Algorithm2();
-		for (String s : str.split(" "))
-			System.out.println(obj.compress(s));
 
 		return result;
 	}
 
 	// 같은 문자가 반복되면 횟수 표시하여 문자열 압축
-	String compress(String s) {
-		String t = "";
-		for (String u : s.split("(?<=(.))(?!\\1)"))
-			t = t + u.charAt(0) + u.length();
-		return t;
+	public static String compress(String s, int startIndex, int endIndex) {
+		String inputTxt = s;
+ 		String resultTxt = "";
+        int count = 0;
+        
+        
+        String temp = inputTxt.substring(startIndex, endIndex);
+        count++;
+        
+        for ( int i = 1; i < inputTxt.length(); i++ ) {
+            
+            if ( inputTxt.substring(startIndex) != temp ) {
+                resultTxt += temp + "" + count;
+                count = 0;
+                
+                temp = inputTxt.substring(startIndex);
+                count++;
+                
+            } else {
+                count++;
+            }
+            
+            if ( i == inputTxt.length()-1 ) {
+                resultTxt += temp + "" + count;
+            }
+        }
+        
+        return resultTxt;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		solution("aabbaccc");
-		// 일단 a2b2a1c3로 출력은 가능ㅠ
+		System.out.println();
 
 	}
 
