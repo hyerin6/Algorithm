df923f18e9b85a6dd69cefe5853abea2edaecd9b
hyerin6 <hyerinn6@gmail.com>
2019-09-23 00:00:29 +0900

    백준 11365

---
+++ course6/src/course6/september/baekjoon/B11365.java
@@ -0,0 +1,18 @@
+package course6.september.baekjoon;
+
+// 백준 11365번 !밀급 급일
+public class B11365 {
+
+	public static void solution(String s) {
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
