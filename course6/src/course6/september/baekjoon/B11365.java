468689408d245a48933c6a2ba7bdc962732b6340
hyerin6 <hyerinn6@gmail.com>
2019-09-23 00:05:47 +0900

    백준 11365

--- course6/src/course6/september/baekjoon/B11365.java
+++ course6/src/course6/september/baekjoon/B11365.java
@@ -1,17 +1,34 @@
 package course6.september.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 백준 11365번 !밀급 급일
 public class B11365 {
 
-	public static void solution(String s) {
-		
+	public static void solution() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while (true) { 
+			String s = null;
+			try {
+				s = reader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+			if (s.equals("END")) 
+				break; 
+			
+			System.out.println(new StringBuffer(s).reverse().toString()); 
+			
+		}
 	}
-	
-	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		solution();
 
 	}
 
