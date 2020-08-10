38aa3bf080eea8bff98a3d4ba26f8fdfc5bd8e23
hyerin6 <hyerinn6@gmail.com>
2020-08-10 23:35:53 +0900

    Baekjoon

---
+++ Baekjoon/src/training/B1158.java
@@ -0,0 +1,38 @@
+package training;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class B1158 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		
+		List<Integer> list = new ArrayList<>();
+		for(int i = 1; i <= n; ++i) {
+			list.add(i);
+		}
+		
+		List<Integer> result = new ArrayList<>();
+		Iterator<Integer> li = list.listIterator();
+		
+		while(li.hasNext()) {
+			
+		}
+
+		
+	
+		
+		
+		
+
+	}
+
+}
