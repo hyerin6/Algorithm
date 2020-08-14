6443155c7b1bb0bce6a1e77645428290b302cda1
hyerin6 <hyerinn6@gmail.com>
2020-08-14 15:52:14 +0900

    1158 요세푸스 문제

--- Baekjoon/src/training/B1158.java
+++ Baekjoon/src/training/B1158.java
@@ -1,38 +1,43 @@
 package training;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class B1158 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		int k = scan.nextInt();
-		
-		List<Integer> list = new ArrayList<>();
-		for(int i = 1; i <= n; ++i) {
-			list.add(i);
-		}
-		
-		List<Integer> result = new ArrayList<>();
-		Iterator<Integer> li = list.listIterator();
-		
-		while(li.hasNext()) {
-			
+		Scanner input = new Scanner(System.in); 
+
+		int n = input.nextInt(); 
+		int m = input.nextInt();
+
+		Queue<Integer> list = new LinkedList<Integer>();
+		Queue<Integer> result = new LinkedList<Integer>();
+
+		for(int i = 0; i < n; ++i){ 
+			list.offer(i + 1); 
 		}
 
-		
-	
-		
-		
-		
+		int count = 1; 
+		while(!list.isEmpty()){ 
+			if(count%m == 0){
+				result.offer(list.poll()); 
+				count++; 
+				continue;
+			} 
+			list.offer(list.poll()); 
+			count++; 
+		} 
+
+		System.out.print("<" + result.poll());
+		while(!result.isEmpty()){
+			System.out.print(", " + result.poll());
+		} 
+		System.out.print(">");
 
 	}
 
 }
+
