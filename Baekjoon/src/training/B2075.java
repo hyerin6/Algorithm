7ecd520cf10bbe343f6a34c3eb1b8f4689846540
hyerin6 <hyerinn6@gmail.com>
2021-01-20 22:53:50 +0900

    N 번째 큰 수

---
+++ Baekjoon/src/training/B2075.java
@@ -0,0 +1,33 @@
+package training;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class B2075 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		Queue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+		for(int i = 0; i < N; ++i) {
+			st = new StringTokenizer(br.readLine());
+			while(st.hasMoreElements()) {
+				q.add(Integer.parseInt(st.nextToken()));
+			}
+		}
+
+		for(int i = 0; i < N-1; ++i) {
+			q.poll();
+		}
+
+		System.out.println(q.poll());
+	}
+}
