eafb08cfe3f3ca093644ab39494d0d18876d1573
hyerin6 <hyerinn6@gmail.com>
2021-04-21 23:54:26 +0900

    백준 알고리즘 : 조합 알고리즘 (재귀호출)

---
+++ Baekjoon/src/bronze2/B2309.java
@@ -0,0 +1,54 @@
+package bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class B2309 {
+	static boolean flag;
+
+	public static void comb(int[] arr, boolean[] visited, int depth, int r, int sum) {
+		if (flag) {
+			return;
+		}
+
+		if (r == 0 && sum == 100) {
+			print(arr, visited);
+			flag = true;
+			return;
+		}
+		if (depth == arr.length) {
+			return;
+		} else {
+			visited[depth] = true;
+			comb(arr, visited, depth + 1, r - 1, sum + arr[depth]);
+			visited[depth] = false;
+			comb(arr, visited, depth + 1, r, sum);
+		}
+	}
+
+	public static void print(int[] arr, boolean[] visited) {
+		List<Integer> result = new ArrayList<>();
+		for (int i = 0; i < 9; ++i) {
+			if (visited[i]) {
+				result.add(arr[i]);
+			}
+		}
+		result.sort(Comparator.comparingInt(x -> x));
+		result.forEach(System.out::println);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] input = new int[9];
+		for (int i = 0; i < 9; ++i) {
+			input[i] = Integer.parseInt(br.readLine());
+		}
+
+		comb(input, new boolean[9], 0, 7, 0);
+	}
+}
