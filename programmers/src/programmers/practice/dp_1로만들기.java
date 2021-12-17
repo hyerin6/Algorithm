c88776ebd435ea0abf928e370d9d878cb5f722ca
hyerin6 <hyerinn6@gmail.com>
2021-12-18 00:39:27 +0900

    Add 다이나믹 프로그래밍

---
+++ programmers/src/programmers/practice/dp_1로만들기.java
@@ -0,0 +1,25 @@
+package programmers.practice;
+
+public class dp_1로만들기 {
+	private static void solution(int x) {
+		int[] dp = new int[x + 1];
+		for (int i = 2; i < x + 1; ++i) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+			if (i % 5 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 5] + 1);
+			}
+		}
+
+		System.out.println(dp[x]);
+	}
+
+	public static void main(String[] args) {
+		solution(26);
+	}
+}
