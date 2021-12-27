6f6b121162f3c556086ba39f7840d16561c05fb5
hyerin6 <hyerinn6@gmail.com>
2021-12-27 15:50:50 +0900

    Add lv2

--- programmers/src/programmers/lv2/후보키.java
+++ programmers/src/programmers/lv2/후보키.java
@@ -1,98 +1,61 @@
 package programmers.lv2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class 후보키 {
-	static int col;
-	static int row;
-	static int[] comb;
-	static String[][] relationStr;
-	static int answer;
-	static List<String> uniqueKeys = new ArrayList<>();
 
-	private static boolean isAlreadyMinKey() {
-		if (uniqueKeys.isEmpty()) {
-			return false;
-		}
-
-		List<Integer> keyDfsList = new ArrayList<>();
-		for (int num : comb) {
-			keyDfsList.add(num);
-		}
-
-		for (String ukl : uniqueKeys) {
-			List<Integer> uniqueKey = new ArrayList<>();
-			String[] array = ukl.split("");
-			for (String str : array) {
-				int num = Integer.parseInt(str);
-				uniqueKey.add(num);
-			}
-			if (keyDfsList.containsAll(uniqueKey)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	private static void check() {
-		Set<String> keyString = new HashSet<>();
-		StringBuilder sb;
-
-		for (String[] arr : relationStr) {
-			sb = new StringBuilder();
-			for (int num : comb) {
-				sb.append(arr[num]);
-			}
-			keyString.add(sb.toString());
-		}
-
-		if (keyString.size() == row) {
-			answer += 1;
-			sb = new StringBuilder();
-			for (int num : comb) {
-				sb.append(num);
-			}
-			uniqueKeys.add(sb.toString());
-		}
-	}
-
-	private static void comb(boolean[] visit, int start, int end) {
-		if (end == 0) {
-			int index = 0;
-			for (int i = 0; i < visit.length; ++i) {
-				if (visit[i]) {
-					comb[index] = i;
-					index++;
-				}
-			}
-			if (!isAlreadyMinKey()) {
-				check();
-			}
-		} else {
-			for (int i = start; i < visit.length; ++i) {
-				visit[i] = true;
-				comb(visit, i + 1, end - 1);
-				visit[i] = false;
-			}
-		}
-	}
+	private static ArrayList<HashSet<Integer>> candidateKey;
 
 	public static int solution(String[][] relation) {
-		answer = 0;
-		col = relation[0].length; // 칼럼 수
-		row = relation.length; // 레코드 수
-		relationStr = relation;
-		for (int i = 0; i < col; ++i) {
-			boolean[] visit = new boolean[col];
-			comb = new int[i + 1];
-			comb(visit, 0, i + 1);
+		candidateKey = new ArrayList<>();
+		int colSize = relation[0].length;
+
+		for (int i = 1; i <= colSize; ++i) {
+			makeKeySet(-1, colSize - 1, 0, i, new HashSet<>(), relation);
 		}
 
-		return answer;
+		return candidateKey.size();
+	}
+
+	private static void makeKeySet(int attr, int maxAttr, int idx, int size, HashSet<Integer> keySet,
+		String[][] relation) {
+		if (idx == size) {
+			for (HashSet<Integer> key : candidateKey) {
+				if (keySet.containsAll(key)) {
+					return;
+				}
+			}
+
+			if (isUnique(keySet, relation)) {
+				candidateKey.add(keySet);
+			}
+			return;
+		}
+
+		for (int i = attr + 1; i <= maxAttr; ++i) {
+			HashSet<Integer> newKeySet = new HashSet<>(keySet);
+			newKeySet.add(i);
+			makeKeySet(i, maxAttr, idx + 1, size, newKeySet, relation);
+		}
+	}
+
+	private static boolean isUnique(HashSet<Integer> keySet, String[][] relation) {
+		HashMap<String, String> map = new HashMap<>();
+
+		for (int r = 0; r < relation.length; ++r) {
+			String key = "";
+			for (int c : keySet) {
+				key += relation[r][c];
+			}
+			if (map.containsKey(key)) {
+				return false;
+			} else {
+				map.put(key, key);
+			}
+		}
+		return true;
 	}
 
 	public static void main(String[] args) {
